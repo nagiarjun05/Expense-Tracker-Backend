09fa0c7fbee69360d36de84bb16b7ed191e7543c
nagiarjun05 <nagiarjun05@gmail.com>
2022-12-16 14:23:30 +0530

    Optimized the leaderboard facility

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,24 +1,46 @@
 const User=require('../models/User');
 const Expense=require('../models/expense');
+const Sequelize=require('sequelize');
+const sequelize=require('../util/database');
 
 const getLeaderboard=async(req, res)=>{
     try{
-        const users=await User.findAll();
-        const expenses=await Expense.findAll();
-        const userAggregateExpense={};
-        expenses.forEach((element) => {
-            if(userAggregateExpense[element.userId]){
-                userAggregateExpense[element.userId]=userAggregateExpense[element.userId]+element.amount;
-            }else{
-                userAggregateExpense[element.userId]=element.amount;
-            }
+
+        const users=await User.findAll({
+            attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.amount')),'total']],
+            include:[
+                {
+                    model: Expense,
+                    attributes:[]
+                }
+            ],
+            group:['user.id'],
+            order:[['total','DESC']]
         });
-        const userLeaderBoard=[];
-        users.forEach((user)=>{
-            userLeaderBoard.push({name: user.name, total: userAggregateExpense[user.id]})
-        });
-        userLeaderBoard.sort((a,b)=> b.total -a.total);
-        res.status(200).json(userLeaderBoard);
+        // const userAggregateExpense=await Expense.findAll({
+        //     attributes: ['userId',[sequelize.fn('sum',sequelize.col('amount')),'total']],
+        //     group: ['userId']
+        // });
+        res.status(200).json(users);
+        
+        
+        // BRUTE FORCE WAY TO SOLVE THIS
+        // users= await User.findAll();
+        // expenses= await Expense.findAll();
+        // const userAggregateExpense={};
+        // expenses.forEach((element) => {
+        //     if(userAggregateExpense[element.userId]){
+        //         userAggregateExpense[element.userId]=userAggregateExpense[element.userId]+element.amount;
+        //     }else{
+        //         userAggregateExpense[element.userId]=element.amount;
+        //     }
+        // });
+        // const userLeaderBoard=[];
+        // users.forEach((user)=>{
+        //     userLeaderBoard.push({name: user.name, total: userAggregateExpense[user.id]||0})
+        // });
+        // userLeaderBoard.sort((a,b)=> b.total -a.total);
+        // res.status(200).json(userLeaderBoard);
     }   
     catch(err){
         res.status(500).json(err);
